2f23423a85ad79c3aabec63c9d1dfff52b4453cd
F. Allocati <fallocati@atioinc.com>
2014-05-10 23:54:40 -0300

    Starting PCA coding

---
+++ src/Components/PrincipalComponentAnalysis/PrincipalComponentAnalysis.cpp
@@ -0,0 +1,2 @@
+#include "PrincipalComponentAnalysis.h"
+
---
+++ src/Components/PrincipalComponentAnalysis/PrincipalComponentAnalysis.h
@@ -0,0 +1,14 @@
+#ifndef PRINCIPAL_COMPONENT_ANALYSIS_H
+#define PRINCIPAL_COMPONENT_ANALYSIS_H
+
+#include <EigenOptimization/Optimization>
+
+class PrincipalComponentAnalysis
+{
+public:
+	PrincipalComponentAnalysis(const Eigen::MatrixXd& trainingSet);
+private:
+	
+};
+
+#endif // PRINCIPAL_COMPONENT_ANALYSIS_H
