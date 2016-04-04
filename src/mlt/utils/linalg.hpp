948c3027178c051834cf5ea2b13cbb9ba9f9b9a0
Federico Allocati <fede.allocati@gmail.com>
2016-04-04 01:24:06 -0300

    Switched to features per row instead of instances per column

--- src/mlt/utils/linalg.hpp
+++ src/mlt/utils/linalg.hpp
@@ -7,7 +7,6 @@
 namespace mlt {
 namespace utils {
 namespace linalg {
-
 	// Moore-Penrose pseudoinverse
 	inline Eigen::MatrixXd
 	pseudo_inverse(const Eigen::MatrixXd& x) {
@@ -21,12 +20,9 @@ namespace linalg {
 	inline Eigen::MatrixXd
 	covariance(const Eigen::MatrixXd& x, const Eigen::MatrixXd& y)
 	{
-		assert(x.rows() == y.rows());
-		const auto num_observations = static_cast<double>(x.rows());
-		const Eigen::RowVectorXd x_mean = x.colwise().sum() / num_observations;
-		const Eigen::RowVectorXd y_mean = y.colwise().sum() / num_observations;
- 
-		return (x.rowwise() - x_mean).transpose() * (y.rowwise() - y_mean) / num_observations;
+		assert(x.cols() == y.cols());
+		const auto num_observations = static_cast<double>(x.cols());
+		return (x.colwise() - (x.rowwise().sum() / num_observations)) * (y.colwise() - (y.rowwise().sum() / num_observations)).transpose() / num_observations;
 	}
 
 	inline Eigen::MatrixXd
