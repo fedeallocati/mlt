d9e8e701551549f8b11768620e98a800afae1e7b
Federico Allocati <fede.allocati@gmail.com>
2016-04-05 03:25:59 -0300

    Fixed constructors of PCA

--- src/mlt/models/transformers/principal_components_analysis.hpp
+++ src/mlt/models/transformers/principal_components_analysis.hpp
@@ -8,7 +8,13 @@ namespace models {
 namespace transformers {
 	class PrincipalComponentsAnalysis : public PrincipalComponentsAnalysisImpl<PrincipalComponentsAnalysis> {
 	public:
-		using PrincipalComponentsAnalysisImpl<PrincipalComponentsAnalysis>::PrincipalComponentsAnalysisImpl;
+		explicit PrincipalComponentsAnalysis(int components_size, bool whiten = false) : PrincipalComponentsAnalysisImpl(components_size, whiten) {}
+
+		explicit PrincipalComponentsAnalysis(double variance_to_retain, bool whiten = false) : PrincipalComponentsAnalysisImpl(variance_to_retain, whiten) {
+			assert(variance_to_retain > 0 && variance_to_retain <= 1);
+		}
+
+		explicit PrincipalComponentsAnalysis(bool whiten = false) : PrincipalComponentsAnalysisImpl(whiten) {}
 	};
 }
 }
