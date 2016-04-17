bae680031e8c2804a9cb38892eacd817962ef9d8
Federico Allocati <fede.allocati@gmail.com>
2016-04-17 20:05:32 -0300

    Draft of Optimizable Linear Regressor. Using examples/main for tests

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -1,5 +1,4 @@
 #define EIGEN_USE_MKL_ALL
-//#define MLT_VERBOSE_TRAINING
 
 #include <chrono>
 
@@ -8,20 +7,25 @@
 //#include "datasets.hpp"
 #include "../mlt/models/regressors/least_squares_linear_regression.hpp"
 #include "../mlt/models/regressors/ridge_regression.hpp"
-#include "../mlt/models/transformations/principal_components_analysis.hpp"
-#include "../mlt/models/transformations/zero_components_analysis.hpp"
+#include "../mlt/models/regressors/optimizable_linear_regressor.hpp"
+#include "../mlt/utils/loss_functions.hpp"
+#include "../mlt/models/transformers/principal_components_analysis.hpp"
+#include "../mlt/models/transformers/zero_components_analysis.hpp"
+#include "../mlt/models/pipeline.hpp"
+
+namespace regressors = mlt::models::regressors;
+namespace linear_solvers = mlt::utils::linear_solvers;
 
 //extern void lr_example(std::tuple<Eigen::MatrixXd, Eigen::MatrixXd> data, Eigen::VectorXd test);
 //extern void pca_example(Eigen::MatrixXd data, Eigen::VectorXd test);
 
 template <typename Model>
-void benchmark(Model& model, Eigen::MatrixXd dataset, int iters)
-{
+void benchmark(Model& model, Eigen::MatrixXd dataset, Eigen::MatrixXd target, int iters) {
 	double min = std::numeric_limits<double>::max(), max = 0, total = 0;
 
 	for (auto it = 0; it < iters; it++) {
 		auto start = std::chrono::steady_clock::now();
-		model.fit(dataset);
+		model.fit(dataset, target);
 		auto end = std::chrono::steady_clock::now();
 		double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();				
 		min = std::min(min, elapsed);
@@ -29,31 +33,100 @@ void benchmark(Model& model, Eigen::MatrixXd dataset, int iters)
 		total += elapsed;
 	}
 
-	std::cout << "Benchmark for " << typeid(Model).raw_name() << " with input (" << dataset.rows() << ", " << dataset.cols() << ") for " << iters << " iterations" << std::endl;
+	std::cout << "Benchmark for " << typeid(Model).name() << " with input (" << dataset.rows() << ", " << dataset.cols() << ") for " << iters << " iterations" << std::endl;
 	std::cout << "Min: " << min << "ms" << std::endl;
 	std::cout << "Max: " << max << "ms" << std::endl;
 	std::cout << "Avg: " << total / iters << "ms" << std::endl;
 	std::cout << "Tot: " << total << "ms" << std::endl;
 }
 
-int main() {
-    /*lr_example(house_value_dataset(), Eigen::Vector3d(1, 1650, 3));
-    lr_example(correlated_data_dataset(1000000), correlatedData(0));
-    pca_example(std::get<0>(house_value_dataset()), Eigen::Vector3d(1, 1650, 3));
-    pca_example(std::get<0>(correlated_data_dataset(1000000)), correlatedData(0));*/
+void benchmark_linear_solvers() {
+	Eigen::MatrixXd XN = Eigen::MatrixXd::Random(100, 100000);
+	Eigen::RowVectorXd YN = Eigen::RowVectorXd::Random(100000);
 
-	Eigen::MatrixXd X1(2, 3);
+	regressors::LeastSquaresLinearRegression<linear_solvers::SVDSolver> linear_regressor_svd(false);
+	benchmark(linear_regressor_svd, XN, YN, 100);
+	regressors::LeastSquaresLinearRegression<linear_solvers::LDLTSolver> linear_regressor_ldlt(false);
+	benchmark(linear_regressor_ldlt, XN, YN, 100);
+	std::cout << std::endl;
+	regressors::LeastSquaresLinearRegression<linear_solvers::CGSolver> linear_regressor_cg(false);
+	benchmark(linear_regressor_cg, XN, YN, 100);
+	std::cout << std::endl;
+
+	std::cout << "Diff: " << (linear_regressor_svd.coefficients() - linear_regressor_ldlt.coefficients()).squaredNorm() << std::endl;
+	std::cout << "Diff: " << (linear_regressor_svd.coefficients() - linear_regressor_cg.coefficients()).squaredNorm() << std::endl;
+	std::cout << "Diff: " << (linear_regressor_ldlt.coefficients() - linear_regressor_cg.coefficients()).squaredNorm() << std::endl;	
+}
+
+std::tuple<double, Eigen::MatrixXd> linear_svm_cost_and_gradient(const Eigen::MatrixXd& beta, const Eigen::MatrixXd& input, const Eigen::MatrixXd& result) {
+	Eigen::MatrixXd scores = input * beta;
+	Eigen::MatrixXd hinge_loss = (((scores.colwise() - scores.cwiseProduct(result).rowwise().sum()) - result).array() + 1).max(0); // 4x3
+	double loss = hinge_loss.sum() / input.rows();
+	/*if (params_t::regularization() > 0) {
+		loss += params_t::regularization() * (betarightCols(beta.cols() - 1).array().pow(2)).sum();
+	}*/
+	Eigen::MatrixXd margin_mask = (hinge_loss.array() > 0).cast<double>();
+	margin_mask = margin_mask + (result.array().colwise() * -margin_mask.rowwise().sum().array()).matrix();
+	Eigen::MatrixXd d_beta = input.transpose() * margin_mask / input.rows();
+	/*if (params_t::regularization() > 0) {
+		d_beta.rightCols(d_beta.cols() - 1) += params_t::regularization() * 2 * beta.rightCols(beta.cols() - 1);
+	}*/
+	return std::make_tuple(loss, d_beta);
+}
+
+Eigen::MatrixXd softmax(const Eigen::MatrixXd& beta, const Eigen::MatrixXd& input) {
+	Eigen::MatrixXd result = input * beta;
+	result.colwise() -= result.rowwise().maxCoeff();
+	result = result.array().exp();
+	result = result.array().colwise() / result.rowwise().sum().array();
+
+	return result;
+}
+
+std::tuple<double, Eigen::MatrixXd> softmax_regression_cost_and_gradient(const Eigen::MatrixXd& beta, const Eigen::MatrixXd& input, const Eigen::MatrixXd& result) {
+	Eigen::MatrixXd scores = softmax(beta, input);
+	double loss = -scores.cwiseProduct(result).colwise().sum().array().log().sum() / input.rows();
+	Eigen::MatrixXd d_beta = ((scores.transpose() * input) - (result.transpose() * input)).transpose() / input.rows();
+	return std::make_tuple(loss, d_beta);
+}
+
+int main() {
+	/*lr_example(house_value_dataset(), Eigen::Vector3d(1, 1650, 3));
+	lr_example(correlated_data_dataset(1000000), correlatedData(0));
+	pca_example(std::get<0>(house_value_dataset()), Eigen::Vector3d(1, 1650, 3));
+	pca_example(std::get<0>(correlated_data_dataset(1000000)), correlatedData(0));*/
+
+	//benchmark_linear_solvers();
+
+	std::cout << "Eigen Version: " << EIGEN_WORLD_VERSION << "." << EIGEN_MAJOR_VERSION << "." << EIGEN_MINOR_VERSION << std::endl;
+	std::cout << "Eigen Threads: " << Eigen::nbThreads() << std::endl;
+	std::cout << "Eigen SIMD Instruction Sets In Use: " << Eigen::SimdInstructionSetsInUse() << std::endl;
+
+#ifdef EIGEN_USE_MKL
+	std::cout << "MKL Version: " << __INTEL_MKL__ << "." << __INTEL_MKL_MINOR__ << "." << __INTEL_MKL_UPDATE__ << std::endl;
+	std::cout << "MKL Threads: " << mkl_get_max_threads();
+	if (MKL_Get_Dynamic())
+		std::cout << " (may use less for large problems)";
+	std::cout << std::endl;
+#endif
+	std::cout << std::endl;
+
+	/*Eigen::MatrixXd X1(2, 3);
 	Eigen::MatrixXd Y1(1, 3);
 
 	X1.row(0) << 0, 1, 2;
 	X1.row(1) << 0, 1, 2;
 	Y1 << 0, 1, 2;
 
-	mlt::models::regressors::LeastSquaresLinearRegression linear_regressor(false);
+	regressors::LeastSquaresLinearRegression<> linear_regressor(false);
 	linear_regressor.fit(X1, Y1);
 
 	std::cout << "LinearRegression: " << std::endl;
 	std::cout << linear_regressor.coefficients() << std::endl;
+	if (linear_regressor.fit_intercept()) {
+		std::cout << linear_regressor.intercepts() << std::endl;
+	}
+	std::cout << linear_regressor.predict(X1.col(0)) << std::endl;
 
 	Eigen::MatrixXd X2(2, 3);
 	Eigen::MatrixXd Y2(1, 3);
@@ -62,19 +135,26 @@ int main() {
 	X2.row(1) << 0, 0, 1;
 	Y2 << 0, .1, 1;
 
-	mlt::models::regressors::RidgeRegression ridge_regressor(0.5, true);
+	regressors::RidgeRegression<> ridge_regressor(0.5, true);
 	ridge_regressor.fit(X2, Y2);
-	
+
 	std::cout << "RidgeRegression: " << std::endl;
 	std::cout << ridge_regressor.coefficients() << std::endl;
 	std::cout << ridge_regressor.intercepts() << std::endl;
-	
+	std::cout << ridge_regressor.predict(X2.col(0)) << std::endl;
+
+	Eigen::MatrixXd XN = Eigen::MatrixXd::Random(100, 100000);
+	Eigen::MatrixXd YN = Eigen::MatrixXd::Random(3, 100000);
+
+	auto pred = ridge_regressor.fit(XN, YN).predict(XN.leftCols<2>());
+	std::cout << "Expected size: 3x2. Obtained size: " << pred.rows() << "x" << pred.cols() << std::endl;
+
 	Eigen::MatrixXd X3(2, 6);
 
 	X3.row(0) << -1, -2, -3, 1, 2, 3;
 	X3.row(1) << -1, -1, -2, 1, 1, 2;
 
-	mlt::models::transformations::PrincipalComponentsAnalysis pca;
+	mlt::models::transformers::PrincipalComponentsAnalysis pca;
 	pca.fit(X3);
 
 	std::cout << "PCA: " << std::endl;
@@ -85,7 +165,7 @@ int main() {
 	std::cout << "PCA(X2[:,1])" << std::endl << pca.transform(X3.leftCols(1)) << std::endl;
 	std::cout << "PCA-1(PCA(X2[:,1]))" << std::endl << pca.inverse_transform(pca.transform(X3.leftCols(1))) << std::endl << std::endl;
 
-	mlt::models::transformations::ZeroComponentsAnalysis zca;
+	mlt::models::transformers::ZeroComponentsAnalysis zca;
 	zca.fit(X3);
 
 	std::cout << "ZCA: " << std::endl;
@@ -94,7 +174,47 @@ int main() {
 	std::cout << "ZCA(X2)" << std::endl << zca.transform(X3) << std::endl;
 	std::cout << "ZCA-1(ZCA(X2))" << std::endl << zca.inverse_transform(zca.transform(X3)) << std::endl;
 	std::cout << "ZCA(X2[:,1])" << std::endl << zca.transform(X3.leftCols(1)) << std::endl;
-	std::cout << "ZCA-1(ZCA(X2[:,1]))" << std::endl << zca.inverse_transform(zca.transform(X3.leftCols(1))) << std::endl << std::endl;
+	std::cout << "ZCA-1(ZCA(X2[:,1]))" << std::endl << zca.inverse_transform(zca.transform(X3.leftCols(1))) << std::endl << std::endl;*/
+
+	/*auto pipeline = mlt::models::create_pipeline(mlt::models::transformers::ZeroComponentsAnalysis(),
+		mlt::models::transformers::PrincipalComponentsAnalysis(0.99), ridge_regressor);*/
+
+	//mlt::utils::optimizers::StochasticGradientDescent<mlt::utils::optimizers::RMSPropGradientDescentUpdate> grad_descent(10, 2000, 0.001, 1, mlt::utils::optimizers::RMSPropGradientDescentUpdate());
+	/*mlt::models::regressors::SquaredLoss<> sgd(0.0,  false);
+	regressors::RidgeRegression<> ridge_regressor2(0.0, false);
+
+	ridge_regressor2.fit(input, output);
+	sgd.fit(input, output, false);
+	
+	std::cout << "RidgeRegression: " << std::endl;
+	std::cout << ridge_regressor2.predict(input) << std::endl;
+	std::cout << "SgdEaeapepe: " << std::endl;
+	std::cout << sgd.predict(input) << std::endl;
+
+	std::cout << "loss with closed form: " << sgd.loss(ridge_regressor2.coefficients(), input, output) << std::endl;
+	std::cout << "loss with SGD: " << sgd.loss(sgd.coefficients(), input, output) << std::endl;*/
+
+	mlt::utils::loss_functions::SoftmaxLoss loss1;
+	mlt::utils::loss_functions::HingeLoss loss2(1.0);
+
+	Eigen::MatrixXd input = Eigen::MatrixXd::Random(3, 10);
+	Eigen::MatrixXd output = Eigen::MatrixXd::Random(2, 10);
+
+	regressors::RidgeRegression<> ridge_regressor2(0.0, false);
+
+	ridge_regressor2.fit(input, output);
+
+	auto a1 = softmax_regression_cost_and_gradient(ridge_regressor2.coefficients().transpose(), input.transpose(), output.transpose());
+	auto b1 = loss1.loss_and_gradient(ridge_regressor2.coefficients() * input, output);
+
+	std::cout << std::get<0>(a1) << " vs " << std::get<0>(b1) << std::endl;
+	std::cout << std::get<1>(a1).transpose() << std::endl << " vs " << std::endl << std::get<1>(b1) * input.transpose() << std::endl;
+
+	auto a2 = linear_svm_cost_and_gradient(ridge_regressor2.coefficients().transpose(), input.transpose(), output.transpose());
+	auto b2 = loss2.loss_and_gradient(ridge_regressor2.coefficients() * input, output);
+
+	std::cout << std::get<0>(a2) << " vs " << std::get<0>(b2) << std::endl;
+	std::cout << std::get<1>(a2).transpose() << std::endl << " vs " << std::endl << std::get<1>(b2) * input.transpose() << std::endl;
 
 	std::cin.get();
 
---
+++ src/mlt/models/regressors/optimizable_linear_regressor.hpp
@@ -0,0 +1,79 @@
+#ifndef MLT_MODELS_REGRESSORS_OPTIMIZABLE_LINEAR_REGRESSOR_HPP
+#define MLT_MODELS_REGRESSORS_OPTIMIZABLE_LINEAR_REGRESSOR_HPP
+
+#include <Eigen/Core>
+
+#include "linear_regressor_model.hpp"
+#include "../../utils/optimizers/stochastic_gradient_descent.hpp"
+
+namespace mlt {
+namespace models {
+namespace regressors {
+	template <class Concrete, class Optimizer>
+	class OptimizableLinearModel {
+	public:	
+		template <class Target>
+		Concrete& fit(const Eigen::MatrixXd& input, const Eigen::MatrixXd& target, bool cold_start) {
+			Eigen::MatrixXd input_prime(input.rows() + (fit_intercept() ? 1 : 0), input.cols());
+			input_prime.topRows(input.rows()) << input;
+
+			if (static_cast<Concrete&>(*this).fit_intercept()) {
+				input_prime.bottomRows<1>() = Eigen::VectorXd::Ones(input.cols());
+			}
+
+			Eigen::MatrixXd coeffs = _optimizer.run(static_cast<Concrete&>(*this), input_prime, target,
+				(this->_fitted && !cold_start ? this->coefficients() : Eigen::MatrixXd::Random(target.rows(), input_prime.rows())), cold_start);
+
+			if (static_cast<Concrete&>(*this).fit_intercept()) {
+				static_cast<Concrete&>(*this)._set_coefficients_and_intercepts(coeffs.leftCols(coeffs.cols() - 1), coeffs.rightCols<1>());
+			}
+			else {
+				static_cast<Concrete&>(*this)._set_coefficients(coeffs);
+			}
+
+			return static_cast<Concrete&>(*this);
+		}
+
+		double loss(const Eigen::Ref<const Eigen::MatrixXd>& params, const Eigen::MatrixXd& input, const Eigen::MatrixXd& result) const {
+			return (((params * input) - result).array().pow(2).sum() / (2 * input.cols())) + _reg * (params.array().pow(2)).sum();
+		}
+
+		std::tuple<double, Eigen::MatrixXd> loss_and_gradient(const Eigen::Ref<const Eigen::MatrixXd>& params, const Eigen::MatrixXd& input, const Eigen::MatrixXd& result) const {
+			Eigen::MatrixXd diff = params * input - result;
+			return std::make_tuple((diff.array().pow(2).sum() / (2 * input.cols())) + 0.5 * (params.array().pow(2)).sum(),
+				((diff * input.transpose()) / input.cols()) + _reg * 2 * params);
+		}
+
+	protected:
+		explicit OptimizableLinearModel(const Optimizer& optimizer,
+			bool fit_intercept = true) : LinearRegressorModel(fit_intercept), _loss(loss), _optimizer(optimizer) {}
+
+		explicit OptimizableLinearModel(Optimizer&& optimizer,
+			bool fit_intercept = true) : LinearRegressorModel(fit_intercept), _optimizer(optimizer) {}
+
+		double _regularization;
+		Optimizer _optimizer;
+	};
+
+	template <class Optimizer = utils::optimizers::StochasticGradientDescent<>>
+	class SquaredLoss : public OptimizableLinearModel<SquaredLoss<Optimizer>, Optimizer> {
+	public:
+		SquaredLoss(double reg, bool fit_intercept = true) : OptimizableLinearRegressor(Optimizer(), fit_intercept), _reg(reg) {}
+
+		SquaredLoss(double reg, const Optimizer& optimizer, bool fit_intercept = true) : OptimizableLinearRegressor(optimizer, fit_intercept), _reg(reg) {}
+
+		double value(const Eigen::Ref<const Eigen::MatrixXd>& params, const Eigen::MatrixXd& input, const Eigen::MatrixXd& result) const {
+			return (((params * input) - result).array().pow(2).sum() / (2 * input.cols())) + _reg * (params.array().pow(2)).sum();
+		}
+
+		std::tuple<double, Eigen::MatrixXd> loss_and_gradient(const Eigen::Ref<const Eigen::MatrixXd>& params, const Eigen::MatrixXd& input, const Eigen::MatrixXd& result) const {
+			Eigen::MatrixXd diff = params * input - result;
+			return std::make_tuple((diff.array().pow(2).sum() / (2 * input.cols())) + 0.5 * (params.array().pow(2)).sum(),
+				((diff * input.transpose()) / input.cols()) + _reg * 2 * params);
+		}
+	};
+
+}
+}
+}
+#endif
