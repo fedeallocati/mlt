29883d0f82841c186764c64cf97f2e8ba361feda
Federico Allocati <fede.allocati@gmail.com>
2016-05-04 03:54:04 -0300

    Added accuracy score to ClassifierMixin and removed commented code

--- src/mlt/models/classifiers/classifier_mixin.hpp
+++ src/mlt/models/classifiers/classifier_mixin.hpp
@@ -10,27 +10,10 @@ namespace classifiers {
 	template <class Classifier>
 	class ClassifierMixin {
 	public:
-		/*double score(X, y, sample_weight = None) {
-			"""Returns the mean accuracy on the given test data and labels.
-				In multi - label classification, this is the subset accuracy
-				which is a harsh metric since you require for each sample that
-				each label set be correctly predicted.
-				Parameters
-				----------
-				X : array - like, shape = (n_samples, n_features)
-				Test samples.
-				y : array - like, shape = (n_samples) or (n_samples, n_outputs)
-				True labels for X.
-				sample_weight : array - like, shape = [n_samples], optional
-				Sample weights.
-				Returns
-				------ -
-				score : float
-				Mean accuracy of self.predict(X) wrt.y.
-				"""
-				from.metrics import accuracy_score
-				return accuracy_score(y, self.predict(X), sample_weight = sample_weight)
-		}*/
+		double score(const Eigen::Ref<const Eigen::MatrixXd>& input, const Eigen::Ref<const Eigen::VectorXi>& classes) const {
+			return (this->_concrete().classify(input).array() == classes.array()).cast<double>().sum() / static_cast<double>(input.cols());
+		}
+
 	protected:
 		ClassifierMixin() {}
 
@@ -61,26 +44,10 @@ namespace classifiers {
 			return classes_vector;
 		}
 
-		/*Classifier& fit(const Eigen::Ref<const Eigen::MatrixXd> input, const Eigen::Ref<const Eigen::MatrixXi>& classes, bool cold_start = true) {
-			assert(input.cols() == classes.cols());
-			assert((classes.rowwise().sum().array() == 1).all());
-
-			std::vector<uint32_t> classes_vector(classes.cols());
-
-			for (size_t col = 0; col < classes.cols(); col++) {
-				for (size_t row = 0; row < classes.rows(); row++) {
-					if (classes(row, col) == 1) {
-						classes_vector[col] = (row);
-						break;
-					}
-				}
-			}
-
-			return this->get_concrete().fit(input, classes_vector, cold_start);
-		}*/
-
 	private:
-		Classifier& _concrete() const { return static_cast<Classifier&>(*this); }
+		const Classifier& _concrete() const { return static_cast<const Classifier&>(*this); }
+
+		Classifier& _concrete() { return static_cast<Classifier&>(*this); }
 	};
 }
 }
